09f96ca77fe18ca0e9a74c0df60cdbc239a7600f
rpokidin <rpokidin@yandex.ru>
2026-02-14 17:54:30 +0300

    3-typescript cmt 3

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.js'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
